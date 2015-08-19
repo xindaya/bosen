6881f992fecccc02018755b13fd3d81a26a65d3c
qiaohaijun <qiaohaijun@gmail.com>
2015-08-19 21:15:03 +0800

    简而言之是用map实现的存储结构

--- app/caffe/ps/src/petuum_ps_common/storage/map_store.hpp
+++ app/caffe/ps/src/petuum_ps_common/storage/map_store.hpp
@@ -9,10 +9,19 @@
 
 namespace petuum {
 
+
+//#
+// 多线程的稀疏行，支持不约束，或者说是无限制的列
+// 这里没有非零的entry在map类型的存储中
+// 如果遇到有0的情况，比如说在ApplyInc 或者ApplyBatchInc中，就会将
+// 0的情况删除
+// #
 // Multi-threaded sparse row, support unbounded # of columns.  There is never
 // non-zero entry in the map storage, as ApplyInc / ApplyBatchInc
 // removes the zero-writes.
-template<typename V>
+
+
+    template<typename V>
 class MapStore : public AbstractStore<V> {
 public:
 
@@ -23,6 +32,9 @@ public:
   MapStore(const MapStore<V> &other):
       data_(other.data_) { }
 
+    //#
+    // & operator 原来是引用啊
+    // #
   MapStore<V> & operator = (const MapStore<V> &other) {
     data_ = other.data_;
     return *this;
@@ -42,11 +54,17 @@ public:
   size_t num_entries() const;
 
 private:
+    //#
+    // 使用无序map来作为data的存储
+    // 1. 简单
+    // 2. 方便
+    // #
   std::unordered_map<int32_t, V> data_;
 };
 
 // ================= Implementation =================
 
+    // 传入的是key，得到的是value，同时对数据类型做了处理
 template<typename V>
 V MapStore<V>::Get(int32_t col_id) const {
   auto it = data_.find(col_id);
@@ -70,6 +88,8 @@ const void MapStore<V>::CopyToVector(void *to) const {
       = reinterpret_cast<std::vector<std::pair<int32_t, V> >*>(to);
   vec->resize(data_.size());
   vec->clear();
+
+    //将数据一条一条的灌入vec中
   for (const auto & pair : data_) {
     vec->push_back(pair);
   }
@@ -80,11 +100,16 @@ size_t MapStore<V>::num_entries() const {
   return data_.size();
 }
 
+//序列化value需要的空间大小
 template<typename V>
 size_t MapStore<V>::SerializedSize() const {
   return data_.size() * (sizeof(int32_t) + sizeof(V));
 }
 
+//#
+// 这个应该没有实现具体的方法，需要todo吧
+// TODO
+// #
 template<typename V>
 size_t MapStore<V>::Serialize(void* bytes) const {
   void* data_ptr = bytes;
