5769729f736ae392447550f7d00aa497a4640211
qiaohaijun <qiaohaijun@gmail.com>
2015-08-19 21:13:42 +0800

    抽象出来最底层数据层的数据存储的结构的迭代工具

--- app/caffe/ps/src/petuum_ps_common/storage/abstract_store_iterator.hpp
+++ app/caffe/ps/src/petuum_ps_common/storage/abstract_store_iterator.hpp
@@ -3,6 +3,13 @@
 #include <boost/noncopyable.hpp>
 #include <stdint.h>
 
+//#
+// 这里使用了boost的noncopyable 类型，说明该类是一个单例对象
+//
+//  这里应该要实现的东西，是对所有的存储的类型的一个操作的抽象
+//  get_key
+//  is_end
+// #
 namespace petuum {
 template<typename V>
 class AbstractIterator : boost::noncopyable {
