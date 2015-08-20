a080e88602c87f14a8d7e72fe71280ef006c03fe
qiaohaijun <qiaohaijun@gmail.com>
2015-08-20 21:53:10 +0800

    对namenode 做了简单分析

--- app/caffe/ps/src/petuum_ps/server/name_node.hpp
+++ app/caffe/ps/src/petuum_ps/server/name_node.hpp
@@ -2,7 +2,9 @@
 
 #include <petuum_ps/server/name_node_thread.hpp>
 #include <pthread.h>
-
+//#定义了几个基本的接口，启动，关闭#
+//#具体定义在name_node_thread.hpp中#
+//#这个pthread_barrier_t 是一个用来同步线程的工具#
 namespace petuum {
 
 class NameNode {
