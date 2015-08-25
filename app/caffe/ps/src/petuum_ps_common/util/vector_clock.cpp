55a54a854a0eba029d8605977cb0c3db755eb6af
qiaohaijun <qiaohaijun@gmail.com>
2015-08-25 19:55:21 +0800

    单线程版本的向量时钟

--- app/caffe/ps/src/petuum_ps_common/util/vector_clock.cpp
+++ app/caffe/ps/src/petuum_ps_common/util/vector_clock.cpp
@@ -1,10 +1,19 @@
 #include <petuum_ps_common/util/vector_clock.hpp>
 
 namespace petuum {
-
+// 来分析一下不是线程安全的版本实现
+// 没有加锁哦
+    // 使用一个无序的map做内部的数据结构
+    // 第一个参数是thread id
+    // 第二个参数是 clock
+    //boost::unordered_map<int32_t, int32_t> vec_clock_; 这段代码是从hpp文件中拷贝的
+    // 我觉得hpp文件中不应该包含具体的变量定义
+    // 对于读者来说这是非常麻烦的事情
+    // 没错，我就是在吐槽
 VectorClock::VectorClock():
   min_clock_(-1){}
 
+    // 初始化，谁也跑不了，都剃成光头，值为0
 VectorClock::VectorClock(const std::vector<int32_t>& ids):
   min_clock_(0) {
   int size = ids.size();
@@ -16,6 +25,7 @@ VectorClock::VectorClock(const std::vector<int32_t>& ids):
   }
 }
 
+    // 注意有一个修改min_clock的动作
 void VectorClock::AddClock(int32_t id, int32_t clock)
 {
   vec_clock_[id] = clock;
@@ -25,6 +35,8 @@ void VectorClock::AddClock(int32_t id, int32_t clock)
   }
 }
 
+// 下面的写法怪怪的
+    // 如果自己是最小的，就连同min_clock_一起更新了
 int32_t VectorClock::Tick(int32_t id)
 {
   if (IsUniqueMin(id)) {
@@ -35,6 +47,7 @@ int32_t VectorClock::Tick(int32_t id)
   return 0;
 }
 
+// 一直更新，直到到达了预定目标
 int32_t VectorClock::TickUntil(int32_t id, int32_t clock) {
   int32_t curr_clock = VectorClock::get_clock(id);
   int32_t num_ticks = clock - curr_clock;
@@ -63,6 +76,7 @@ int32_t VectorClock::get_min_clock() const
 
 // =========== Private Functions ============
 
+    // 检查唯一性的方法就是遍历一遍数据，统计和min_clock相等的个数
 bool VectorClock::IsUniqueMin(int32_t id)
 {
   if (vec_clock_[id] != min_clock_) {
