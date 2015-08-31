b2efa1c481e229199a0f14b6307b8c136ee201b8
qiaohaijun <qiaohaijun@gmail.com>
2015-09-01 00:00:18 +0800

    对几个关键重要的变量做了注释

--- app/caffe/src/caffe/syncedmem.cpp
+++ app/caffe/src/caffe/syncedmem.cpp
@@ -24,6 +24,7 @@ inline void SyncedMemory::to_cpu() {
     CaffeMallocHost(&cpu_ptr_, size_);
     caffe_memset(size_, 0, cpu_ptr_);
     head_ = HEAD_AT_CPU;
+          // own_cpu_data 这里的意思是说,cpu_ptr_ 是否在持有自己的通过malloc这样的内存申请工作获取的内存
     own_cpu_data_ = true;
     break;
   case HEAD_AT_GPU:
@@ -48,6 +49,10 @@ inline void SyncedMemory::to_gpu() {
 #ifndef CPU_ONLY
   switch (head_) {
   case UNINITIALIZED:
+
+    // 整个caffe的内存的申请工作的起点,lazily,当用到的时候才会真正的做内存申请的工作
+    // syncedmem初始化的时候会将HEAD赋值为UNINITIALIZED
+    
     CUDA_CHECK(cudaMalloc(&gpu_ptr_, size_));
     caffe_gpu_memset(size_, 0, gpu_ptr_);
     head_ = HEAD_AT_GPU;
